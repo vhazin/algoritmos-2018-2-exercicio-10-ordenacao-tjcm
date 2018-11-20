8ca750168d5227dcd80cfbbbc4a716b5631c80d9
Thiago <tjcm@cesar.school>
2018-11-20 08:34:51 -0300

    projeto iniciado

---
+++ thiagomoraes-ord-uri/thiagomoraes-ord-uri/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  thiagomoraes-ord-uri
+//
+//  Created by Thiago Moraes on 19/11/18.
+//  Copyright © 2018 Thiago Moraes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
